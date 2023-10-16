43db99648d3d8822cf428f784d54f6e48ddce28f
Sadim Mali <sadimmalakar77@gmail.com>
2023-10-16 08:07:04 +0545

    feat : dynamically add item & render content according to routes

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 const port = 3000;
-app.use(express.static("public"));
+let todayItems = ["Wake-up", "Code", "Eat", "Sleep","Repeat"];
+let workItems = ["Code", "Error", "Debug","Refator"];
 
+//Middleware
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const date = new Date;
+const currentDay = date.toDateString();
+//Routes
 app.get('/', (req, res)=>{
-    const date = new Date;
-    const currentDay = date.toDateString();
-    res.render("index.ejs",{ todayDate: currentDay});
+    res.render("index.ejs",{ listTitle: currentDay, newItem : todayItems});
 })
+
 app.get('/work', (req, res)=>{
-    res.render("work.ejs");
+    res.render("index.ejs",{listTitle : "Work Title", newItem : workItems});
 })
+
+app.post('/', (req, res)=>{
+    let item = req.body["newItem"];
+    const btn = req.body["list"];
+    console.log(btn);
+     if (btn === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        todayItems.push(item);
+        res.redirect("/");
+    }
+});
 
 app.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
